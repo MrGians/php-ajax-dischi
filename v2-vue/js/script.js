0da4b225ccded6fc34e32d2c64333d4c5e7b15f7
MrGians <gianlumura97@gmail.com>
2022-09-01 17:53:52 +0200

    fix

--- v2-vue/js/script.js
+++ v2-vue/js/script.js
@@ -9,12 +9,14 @@ const app = new Vue({
   },
   methods: {
     fetchDiscs() {
-      axios.get(`php-ajax-dischi/api/discs?genre=${this.genreChoice}`).then((res) => {
-        this.discs = res.data;
-      });
+      axios
+        .get(`http://localhost/php-ajax-dischi/api/discs?genre=${this.genreChoice}`)
+        .then((res) => {
+          this.discs = res.data;
+
+          this.getGenreList();
+        });
     },
-  },
-  computed: {
     getGenreList() {
       this.discs.filter((disc) => {
         if (!this.genreList.includes(disc.genre)) return this.genreList.push(disc.genre);
